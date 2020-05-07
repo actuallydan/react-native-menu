3715163750a27d909e46336bcc5f3c5de235a873
actuallyDan <dankral01@gmail.com>
2020-05-07 13:13:34 -0400

    added custom styles prop and closeDrawer on tap overlay

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useRef, useEffect, useCallback } from "react";
 import {
   Animated,
   Dimensions,
@@ -7,191 +7,206 @@ import {
   Text,
   SafeAreaView,
   Platform,
-  TouchableOpacity
-} from "react-native"
-import PropTypes from "prop-types"
+  TouchableOpacity,
+} from "react-native";
+import PropTypes from "prop-types";
 
-const SCREEN_WIDTH = Dimensions.get("window").width
-const SCREEN_HEIGHT = Dimensions.get("window").height
-const isIOS = Platform.OS === "ios"
-const VERSION = parseInt(Platform.Version, 10)
+const SCREEN_WIDTH = Dimensions.get("window").width;
+const SCREEN_HEIGHT = Dimensions.get("window").height;
+const isIOS = Platform.OS === "ios";
+const VERSION = parseInt(Platform.Version, 10);
 
-class MenuDrawer extends React.Component {
-  constructor(props) {
-    super(props)
-    this.leftOffset = new Animated.Value(0)
-    this.state = {
-      expanded: false,
-      fadeAnim: new Animated.Value(1)
-    }
-  }
+function MenuDrawer({
+  open,
+  drawerContent,
+  drawerPercentage,
+  animationTime,
+  overlay,
+  opacity,
+  children,
+  containerViewStyles = {},
+}) {
+  let leftOffset = useRef(new Animated.Value(0));
+  const [expanded, setExpanded] = useState(false);
+  const [fadeAnim, setFadeAnim] = useState(new Animated.Value(1));
 
-  openDrawer = () => {
-    const { drawerPercentage, animationTime, opacity } = this.props
-    const DRAWER_WIDTH = SCREEN_WIDTH * (drawerPercentage / 100)
+  const openDrawer = useCallback(() => {
+    const DRAWER_WIDTH = SCREEN_WIDTH * (drawerPercentage / 100);
 
     Animated.parallel([
-      Animated.timing(this.leftOffset, {
+      Animated.timing(leftOffset.current, {
         toValue: DRAWER_WIDTH,
         duration: animationTime,
-        useNativeDriver: true
+        useNativeDriver: true,
       }),
-      Animated.timing(this.state.fadeAnim, {
+      Animated.timing(fadeAnim, {
         toValue: opacity,
         duration: animationTime,
-        useNativeDriver: true
-      })
-    ]).start()
-  }
+        useNativeDriver: true,
+      }),
+    ]).start();
+  }, [leftOffset, animationTime, fadeAnim, opacity, drawerPercentage]);
 
-  closeDrawer = () => {
-    const { animationTime } = this.props
-
+  const closeDrawer = useCallback(() => {
     Animated.parallel([
-      Animated.timing(this.leftOffset, {
+      Animated.timing(leftOffset.current, {
         toValue: 0,
         duration: animationTime,
-        useNativeDriver: true
+        useNativeDriver: true,
       }),
-      Animated.timing(this.state.fadeAnim, {
+      Animated.timing(fadeAnim, {
         toValue: 1,
         duration: animationTime,
-        useNativeDriver: true
-      })
-    ]).start()
-  }
+        useNativeDriver: true,
+      }),
+    ]).start();
+  }, [leftOffset, animationTime, fadeAnim]);
 
-  drawerFallback = () => {
+  const drawerFallback = () => {
     return (
-      <TouchableOpacity onPress={this.closeDrawer}>
+      <TouchableOpacity onPress={closeDrawer}>
         <Text>Close</Text>
       </TouchableOpacity>
-    )
-  }
+    );
+  };
 
-  componentDidUpdate() {
-    const { open } = this.props
+  useEffect(() => {
+    open ? openDrawer() : closeDrawer();
+  }, [open, openDrawer, closeDrawer]);
 
-    open ? this.openDrawer() : this.closeDrawer()
-  }
+  const renderPush = () => {
+    const animated = { transform: [{ translateX: leftOffset.current }] };
+    const DRAWER_WIDTH = SCREEN_WIDTH * (drawerPercentage / 100);
 
-  renderPush = () => {
-    const { children, drawerContent, drawerPercentage } = this.props
-    const { fadeAnim } = this.state
-    const animated = { transform: [{ translateX: this.leftOffset }] }
-    const DRAWER_WIDTH = SCREEN_WIDTH * (drawerPercentage / 100)
+    const AnimatedTouchable = Animated.createAnimatedComponent(
+      TouchableOpacity
+    );
 
     if (isIOS && VERSION >= 11) {
       return (
-        <Animated.View style={[animated, styles.main]}>
+        <Animated.View style={[animated, styles.main, containerViewStyles]}>
           <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
             <View
               style={[
                 styles.drawer,
                 {
                   width: DRAWER_WIDTH,
-                  left: -DRAWER_WIDTH
-                }
+                  left: -DRAWER_WIDTH,
+                },
               ]}
             >
-              {drawerContent ? drawerContent : this.drawerFallback()}
+              {drawerContent ? drawerContent : drawerFallback()}
             </View>
-            <Animated.View
+            <AnimatedTouchable
+              onPress={closeDrawer}
               style={[
                 styles.container,
                 {
-                  opacity: fadeAnim
-                }
+                  opacity: fadeAnim,
+                },
               ]}
             >
               {children}
-            </Animated.View>
+            </AnimatedTouchable>
           </SafeAreaView>
         </Animated.View>
-      )
+      );
     }
 
     return (
-      <Animated.View style={[animated, styles.main, { width: SCREEN_WIDTH + DRAWER_WIDTH }]}>
+      <Animated.View
+        style={[
+          animated,
+          styles.main,
+          containerViewStyles,
+          { width: SCREEN_WIDTH + DRAWER_WIDTH },
+        ]}
+      >
         <View
           style={[
             styles.drawer,
             {
               width: DRAWER_WIDTH,
-              left: -DRAWER_WIDTH
-            }
+              left: -DRAWER_WIDTH,
+            },
           ]}
         >
-          {drawerContent ? drawerContent : this.drawerFallback()}
+          {drawerContent ? drawerContent : drawerFallback()}
         </View>
-        <Animated.View
+        <AnimatedTouchable
+          onPress={closeDrawer}
           style={[
             styles.container,
             {
-              opacity: fadeAnim
-            }
+              opacity: fadeAnim,
+            },
           ]}
         >
           {children}
-        </Animated.View>
+        </AnimatedTouchable>
       </Animated.View>
-    )
-  }
+    );
+  };
 
-  renderOverlay = () => {
-    const { children, drawerContent, drawerPercentage } = this.props
-    const { fadeAnim } = this.state
-    const animated = { transform: [{ translateX: this.leftOffset }] }
-    const DRAWER_WIDTH = SCREEN_WIDTH * (drawerPercentage / 100)
+  const renderOverlay = () => {
+    const animated = { transform: [{ translateX: leftOffset.current }] };
+    const DRAWER_WIDTH = SCREEN_WIDTH * (drawerPercentage / 100);
+    const AnimatedTouchable = Animated.createAnimatedComponent(
+      TouchableOpacity
+    );
 
     if (isIOS && VERSION >= 11) {
       return (
-        <SafeAreaView style={[styles.main]}>
+        <SafeAreaView style={[styles.main, containerViewStyles]}>
           <Animated.View
-            style={[animated, styles.drawer, { width: DRAWER_WIDTH, left: -DRAWER_WIDTH }]}
+            style={[
+              animated,
+              styles.drawer,
+              { width: DRAWER_WIDTH, left: -DRAWER_WIDTH },
+            ]}
           >
-            {drawerContent ? drawerContent : this.drawerFallback()}
+            {drawerContent ? drawerContent : drawerFallback()}
           </Animated.View>
-          <Animated.View style={[styles.container, { opacity: fadeAnim }]}>
+          <AnimatedTouchable
+            onPress={closeDrawer}
+            style={[styles.container, { opacity: fadeAnim }]}
+          >
             {children}
-          </Animated.View>
+          </AnimatedTouchable>
         </SafeAreaView>
-      )
+      );
     }
 
     return (
-      <View style={styles.main}>
+      <View style={[styles.main, containerViewStyles]}>
         <Animated.View
           style={[
             animated,
             styles.drawer,
             {
               width: DRAWER_WIDTH,
-              left: -DRAWER_WIDTH
-            }
+              left: -DRAWER_WIDTH,
+            },
           ]}
         >
-          {drawerContent ? drawerContent : this.drawerFallback()}
+          {drawerContent ? drawerContent : drawerFallback()}
         </Animated.View>
-        <Animated.View
+        <AnimatedTouchable
+          onPress={closeDrawer}
           style={[
             styles.container,
             {
-              opacity: fadeAnim
-            }
+              opacity: fadeAnim,
+            },
           ]}
         >
           {children}
-        </Animated.View>
+        </AnimatedTouchable>
       </View>
-    )
-  }
+    );
+  };
 
-  render() {
-    const { overlay } = this.props
-
-    return overlay ? this.renderOverlay() : this.renderPush()
-  }
+  return overlay ? renderOverlay() : renderPush();
 }
 
 MenuDrawer.defaultProps = {
@@ -199,35 +214,35 @@ MenuDrawer.defaultProps = {
   drawerPercentage: 45,
   animationTime: 200,
   overlay: true,
-  opacity: 0.4
-}
+  opacity: 0.4,
+};
 
 MenuDrawer.propTypes = {
   open: PropTypes.bool,
   drawerPercentage: PropTypes.number,
   animationTime: PropTypes.number,
   overlay: PropTypes.bool,
-  opacity: PropTypes.number
-}
+  opacity: PropTypes.number,
+};
 
 const styles = StyleSheet.create({
   main: {
     position: "absolute",
     left: 0,
-    top: 5
+    top: 0,
   },
   container: {
     position: "absolute",
     left: 0,
     width: SCREEN_WIDTH,
     height: SCREEN_HEIGHT,
-    zIndex: 0
+    zIndex: 0,
   },
   drawer: {
     position: "absolute",
     height: SCREEN_HEIGHT,
-    zIndex: 1
-  }
-})
+    zIndex: 1,
+  },
+});
 
-export default MenuDrawer
+export default MenuDrawer;
